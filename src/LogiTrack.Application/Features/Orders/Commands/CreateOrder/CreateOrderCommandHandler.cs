a661f0b80bf776b43ef3aa5e67b21b4c8f4b3e1d
Shefat Al Mahmud <shefat2002@gmail.com>
2026-06-19 22:03:42 +0600

    Update CreateOrderCommandHandler.cs

--- src/LogiTrack.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ src/LogiTrack.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,6 +1,28 @@
+using LogiTrack.Application.Common.Interfaces;
+using LogiTrack.Domain.Entities;
+using MediatR;
+
 namespace LogiTrack.Application.Features.Orders.Commands.CreateOrder;
 
-public class CreateOrderCommandHandler
+public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Guid>
 {
-    
+    private readonly IApplicationDbContext _context;
+    public CreateOrderCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Guid> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = new Order(
+            request.CustomerId,
+            request.PickupLocation,
+            request.DropoffLocation,
+            request.Price);
+        
+        _context.Orders.Add(order);
+        
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return order.Id;
+    }
 }
